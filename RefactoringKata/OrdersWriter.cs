8d2f27251d53361842a36333d05b803e8aa8bd59
phillip.ko38 <phillipko38@outlook.com>
2017-03-07 21:24:14 +0800

    Refactored: extract method

--- RefactoringKata/OrdersWriter.cs
+++ RefactoringKata/OrdersWriter.cs
@@ -14,38 +14,41 @@ namespace RefactoringKata
         public string GetContents()
         {
             var sb = new StringBuilder("{\"orders\": [");
-
             for (var i = 0; i < _orders.GetOrdersCount(); i++)
             {
-                var order = _orders.GetOrder(i);
-                sb.AppendFormat("{0}\"id\": {1}, \"products\": [", "{",  order.GetOrderId());
-
-                for (var j = 0; j < order.GetProductsCount(); j++)
-                {
-                    var product = order.GetProduct(j);
-                    sb.AppendFormat("{0}\"code\": \"{1}\", \"color\": \"{2}\", ", "{", product.Code, GetColorFor(product));
-
-                    if (product.Size != Product.SIZE_NOT_APPLICABLE)
-                    {
-                        sb.AppendFormat("\"size\": \"{0}\", ", GetSizeFor(product));
-                    }
-
-                    sb.AppendFormat("\"price\": {0}, \"currency\": \"{1}\"{2}, ", product.Price, product.Currency, "}");
-                }
-
-                if (order.GetProductsCount() > 0)
-                {
-                    sb.Remove(sb.Length - 2, 2);
-                }
-                sb.AppendFormat("]{0}, ", "}");
+                GetOrder(i, sb);
             }
-
             if (_orders.GetOrdersCount() > 0)
             {
                 sb.Remove(sb.Length - 2, 2);
             }
-
             return sb.Append("]}").ToString();
+        }
+
+        private void GetOrder(int i, StringBuilder sb)
+        {
+            var order = _orders.GetOrder(i);
+            sb.AppendFormat("{0}\"id\": {1}, \"products\": [", "{", order.GetOrderId());
+            for (var j = 0; j < order.GetProductsCount(); j++)
+            {
+                GetProduct(sb, order, j);
+            }
+            if (order.GetProductsCount() > 0)
+            {
+                sb.Remove(sb.Length - 2, 2);
+            }
+            sb.AppendFormat("]{0}, ", "}");
+        }
+
+        private static void GetProduct(StringBuilder sb, Order order, int j)
+        {
+            var product = order.GetProduct(j);
+            sb.AppendFormat("{0}\"code\": \"{1}\", \"color\": \"{2}\", ", "{", product.Code, GetColorFor(product));
+            if (product.Size != Product.SIZE_NOT_APPLICABLE)
+            {
+                sb.AppendFormat("\"size\": \"{0}\", ", GetSizeFor(product));
+            }
+            sb.AppendFormat("\"price\": {0}, \"currency\": \"{1}\"{2}, ", product.Price, product.Currency, "}");
         }
 
 
